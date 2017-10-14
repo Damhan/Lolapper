1bdee997e88e0e19d906b3572346d7ef57ad87b7
Damhan Richardson <damhanrichardson@gmail.com>
2017-10-14 19:39:53 +0100

    Removed test code

--- lolapper/lolapper.py
+++ lolapper/lolapper.py
@@ -6,9 +6,3 @@ class Lolapper(object):
         self.baseApi = BaseApi(key)
         self.champion =  ChampionV3Api(self.baseApi)
         
-def main():
-	testWrapper = Lolapper("RGAPI-9bc7ab7f-555e-4946-a916-b8cb32c16e2b")
-	print(testWrapper.champion.byId(10))
-
-if __name__ == "__main__":
-	main()
